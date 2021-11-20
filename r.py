9a8cf3dde1f3312350ebed122c71d45c9a742eb9
arpita-lataye <89316522+arpita-lataye@users.noreply.github.com>
2021-11-20 23:25:30 +0530

    Add files via upload

---
+++ r.py
@@ -0,0 +1,17 @@
+x=int(input('enter number:'))
+y=int(input('enter number:'))
+z=int(input('enter number:'))
+if (x>y and x>z):
+    print(x,'is greater')
+if (y>x and y>z):
+    print(y,'is greater')
+if (z>x and z>y):
+    print(z,'is greater')
+if (x<y and x<z):
+    print(x,'is smaller')
+if (y<x and y<z):
+    print(y,'is smaller')
+if (z<x and z<y):
+    print(z,'is smaller')
+    
+
